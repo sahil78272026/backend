af695b9f5f54137c9ad5ef7982301e8e6bc68955
Sahil Garg <sahil.garg@crosslynxus.com>
2024-04-29 16:11:53 +0530

    unit testing

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Recipe
 
-# Create your tests here.
+class ProductTestCase(TestCase):
+    def test_product_creation(self):
+        product = Recipe.objects.create(
+            name='Test Product',
+            desc='This is a test product.'
+        )
+        self.assertEqual(product.name, 'Test Product')
+        self.assertEqual(product.desc, 'This is a test product.')
